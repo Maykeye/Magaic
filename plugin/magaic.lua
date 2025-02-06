c53d84fdc026c495140c08b8f7c09dcc50169ec0
Maykeye <Maykeye@users.noreply.github.com>
2025-02-06 09:50:31 +0600

    Fixed keybindings

--- plugin/magaic.lua
+++ plugin/magaic.lua
@@ -9,9 +9,9 @@ local function setup()
     vim.api.nvim_create_user_command("MagicShow", magaic.display_buffer, { nargs=0, desc="Show magaic buffer" })
     vim.api.nvim_create_user_command("MagicChangeModel", magaic.change_model, { nargs=0, desc="Change the model" })
     vim.keymap.set("n", "<leader>ms", magaic.display_buffer, {desc = "Show magic buffer"})
-    vim.keymap.set("n", "<leader>mc", magaic.display_buffer, {desc = "Chat"})
-    vim.keymap.set("n", "<leader>mr", magaic.display_buffer, {desc = "Raw completition"})
-    vim.keymap.set("n", "<leader>mm", magaic.display_buffer, {desc = "Look/change model"})
+    vim.keymap.set("n", "<leader>mc", magaic.chat_completion, {desc = "Chat"})
+    vim.keymap.set("n", "<leader>mr", magaic.general_completion, {desc = "Raw completition"})
+    vim.keymap.set("n", "<leader>mm", magaic.change_model, {desc = "Look/change model"})
     local wk = require("which-key");
     if wk ~= nil then
         wk.register {
