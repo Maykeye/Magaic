ad23f35eae4a079cc135097bd5386400e0fec33f
Maykeye <Maykeye@users.noreply.github.com>
2025-08-01 12:52:29 +0600

    Cleanup: no more glob vars

--- python/llama.cpp-raw-query.py
+++ python/llama.cpp-raw-query.py
@@ -46,7 +46,8 @@ def create_table(conn):
     )
 
 
-def log(ctx: Context, model, mode, request, response, error):
+def log(ctx: Context, model, request, response, error):
+    mode = "chat" if ctx.use_prompt else "raw"
     if not ctx.logging_path:
         return
     try:
@@ -67,14 +68,11 @@ def convert_prompt(ctx: Context):
         return ctx.input_prompt
 
     def apply_role(role):
-        nonlocal lines
-        nonlocal line
-        nonlocal i
-        line = line[line.find(">") + 1 :]
-        line = line.removeprefix(":")
-        line = line.removeprefix(" ")
-        line = f"<|im_start|>{role}\n{line}"
-        lines[i] = line
+        new_line = line[line.find(">") + 1 :]
+        new_line = new_line.removeprefix(":")
+        new_line = new_line.removeprefix(" ")
+        new_line = f"<|im_start|>{role}\n{new_line}"
+        lines[i] = new_line
         if i:
             lines[i - 1] += "<|im_end|>"
         return role
@@ -129,65 +127,65 @@ def parse_args() -> Context:
             ctx.input_prompt = sys.argv[1]
     else:
         ctx.input_prompt = "<sys>:You are helpful assistant, your theme is touhou lore.\n<USR>who is Marisa"
+    if ctx.use_tee:
+        assert ctx.filename, "Filename required for a tee mode"
+        ctx.file_dst = open(ctx.filename, "a")
     return ctx
 
 
-ctx = parse_args()
+def main():
+    ctx = parse_args()
+    prompt = convert_prompt(ctx)
+    request_data = {
+        "prompt": prompt,
+        "stream": True,
+        "params": {"n_predict": ctx.n_predict},
+    }
 
+    response = requests.get(ctx.url("/props"))
+    props = response.content.decode()
+    props_dict = json.loads(props)
+    model_id = props_dict["model_path"]
+    print(prompt)
+    log(ctx, model_id, json.dumps(request_data), "<init>", None)
 
-if ctx.use_tee:
-    assert ctx.filename, "Filename required for a tee mode"
-    ctx.file_dst = open(ctx.filename, "a")
-
-prompt = convert_prompt(ctx)
-request_data = {
-    "prompt": prompt,
-    "stream": True,
-    "params": {"n_predict": ctx.n_predict},
-}
-
-response = requests.get(ctx.url("/props"))
-props = response.content.decode()
-props_dict = json.loads(props)
-model_id = props_dict["model_path"]
-print(prompt)
-
-total_response = ""
-error_message = None
-current_line = ""
-received = 0
-line_mode = True
-try:
-    response = requests.post(ctx.url("/completion"), json=request_data, stream=True)
-    response.raise_for_status()
-    for bdata in response.iter_lines():
-        # iter_lines(decode_unicode=True incorrectly assumes encodeding)
-        data = bdata.decode().strip().removeprefix("data: ")
-        if not data:
-            continue
-        content = json.loads(data)["content"]
-        total_response += content
-        print(content, flush=True, end="")
+    total_response = ""
+    error_message = None
+    received = 0
+    try:
+        response = requests.post(ctx.url("/completion"), json=request_data, stream=True)
+        response.raise_for_status()
+        for bdata in response.iter_lines():
+            # iter_lines(decode_unicode=True incorrectly assumes encodeding)
+            data = bdata.decode().strip().removeprefix("data: ")
+            if not data:
+                continue
+            content = json.loads(data)["content"]
+            total_response += content
+            print(content, flush=True, end="")
+            if ctx.file_dst:
+                ctx.file_dst.write(content)
+                ctx.file_dst.flush()
+            received += 1
+            if ctx.n_predict and received >= ctx.n_predict:
+                response.close()
+                break
+    except KeyboardInterrupt:
+        total_response += "(abort)"
         if ctx.file_dst:
-            ctx.file_dst.write(content)
-            ctx.file_dst.flush()
-        received += 1
-        if ctx.n_predict and received >= ctx.n_predict:
-            response.close()
-            break
-    print(current_line)
-except KeyboardInterrupt:
-    total_response += "(abort)"
-    print("(abort)")
-    error_message = "KeyboardInterrupt"
-except Exception as e:
-    error_message = f"{e}"
-    raise e
-finally:
-    ctx.close_dst_file()
+            ctx.file_dst.write("(abort)")
+        print("(abort)")
+        error_message = "KeyboardInterrupt"
+    except Exception as e:
+        error_message = f"{e}"
+        raise e
+    finally:
+        ctx.close_dst_file()
 
-mode = "chat" if ctx.use_prompt else "raw"
-request_data["input_prompt"] = ctx.input_prompt
-request_data["raw_prompt"] = prompt
-log(ctx, model_id, mode, json.dumps(request_data), total_response, error_message)
+    request_data["input_prompt"] = ctx.input_prompt
+    log(ctx, model_id, json.dumps(request_data), total_response, error_message)
+
+
+if __name__ == "__main__":
+    main()
 
